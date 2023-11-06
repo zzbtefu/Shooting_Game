b5863eae14df9c9ad3ea0739e2a79beea0345e61
Computer Seminar I <anonymous@example.org>
2023-11-06 14:39:55 +0900

    make each function

--- particle/particle.py
+++ particle/particle.py
@@ -6,20 +6,39 @@ class Particle:
     pass
 
 
+def init_particle(p, pos, vel):
+    p.is_alive = True
+    p.x, p.y = pos
+    p.vx, p.vy = vel
+
+
+def update_particle(p, width, height, dt, gy):
+    p.vy += gy * dt
+    p.x += p.vx * dt
+    p.y += p.vy * dt
+    if p.x < 0 or p.x > width or p.y > height:
+        p.is_alive = False
+
+
+def draw_particle(p, screen):
+    radius = 10
+    pygame.draw.circle(screen, pygame.Color("green"), (p.x, p.y), radius)
+
+
 def main():
     pygame.init()
     width, height = 600, 400
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
-    
+
+    dt = 1.0
+    gy = 0.5
+    particle_list = []
+
     while True:
         frames_per_second = 60
         clock.tick(frames_per_second)
-        
-        dt = 1.0
-        gy = 0.5
-        particle_list = []
-        
+
         should_quit = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -30,29 +49,23 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     p = Particle()
-                    p.is_alive = True
-                    p.x, p.y = event.pos
-                    p.vx = random.uniform(-10, 10)
-                    p.vy = random.uniform(-10, 0)
+                    vx = random.uniform(-10, 10)
+                    vy = random.uniform(-10, 0)
+                    init_particle(p, event.pos, (vx, vy))
                     particle_list.append(p)
         if should_quit:
             break
-        
+
         for p in particle_list:
-            p.vy += gy * dt
-            p.x += p.vx * dt
-            p.y += p.vy * dt
-            if p.x < 0 or p.x > width or p.y > height:
-                p.is_alive = False
-                
+            update_particle(p, width, height, dt, gy)
+
         particle_list[:] = [p for p in particle_list if p.is_alive]
-            
+
         screen.fill(pygame.Color("black"))
         for p in particle_list:
-            radius = 10
-            pygame.draw.circle(screen, pygame.Color("green"), (p.x, p.y), radius)
+            draw_particle(p, screen)
         pygame.display.update()
-        
+
     pygame.quit()
 
 
