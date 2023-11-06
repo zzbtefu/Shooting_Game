e55eda856d6afcdc7af7368c07428f6d32dd72cf
Computer Seminar I <anonymous@example.org>
2023-11-06 15:39:45 +0900

    make the main to class and use "run"

--- particle/particle.py
+++ particle/particle.py
@@ -2,17 +2,25 @@ import random
 import pygame
 
 
+class World:
+    def __init__(self, width, height, dt, gy):
+        self.width = width
+        self.height = height
+        self.dt = dt
+        self.gy = gy
+
 class Particle:
-    def __init__(self, pos, vel):
+    def __init__(self, pos, vel, world):
         self.is_alive = True
         self.x, self.y = pos
         self.vx, self.vy = vel
+        self.world = world
 
-    def update(self, width, height, dt, gy):
-        self.vy += gy * dt
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-        if self.x < 0 or self.x > width or self.y > height:
+    def update(self):
+        self.vy += self.world.gy * self.world.dt
+        self.x += self.vx * self.world.dt
+        self.y += self.vy * self.world.dt
+        if self.x < 0 or self.x > self.world.width or self.y > self.world.height:
             self.is_alive = False
 
     def draw(self, screen):
@@ -20,49 +28,49 @@ class Particle:
         pygame.draw.circle(screen, pygame.Color("green"), (self.x, self.y), radius)
 
 
-def main():
-    pygame.init()
-    width, height = 600, 400
-    screen = pygame.display.set_mode((width, height))
-    clock = pygame.time.Clock()
+class AppMain:
+    def run(self):
+        pygame.init()
+        width, height = 600, 400
+        screen = pygame.display.set_mode((width, height))
+        clock = pygame.time.Clock()
 
-    dt = 1.0
-    gy = 0.5
-    particle_list = []
+        world = World(width, height, dt=1.0, gy=0.5)
+        particle_list = []
 
-    while True:
-        frames_per_second = 60
-        clock.tick(frames_per_second)
+        while True:
+            frames_per_second = 60
+            clock.tick(frames_per_second)
 
-        should_quit = False
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                should_quit = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+            should_quit = False
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     should_quit = True
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        should_quit = True
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        vx = random.uniform(-10, 10)
+                        vy = random.uniform(-10, 0)
+                        p = Particle(event.pos, (vx, vy), world)
+                        particle_list.append(p)
+            if should_quit:
+                break
 
-                    vx = random.uniform(-10, 10)
-                    vy = random.uniform(-10, 0)
-                    p = Particle(event.pos, (vx, vy))
-                    particle_list.append(p)
-        if should_quit:
-            break
+            for p in particle_list:
+                p.update()
 
-        for p in particle_list:
-            p.update(width, height, dt, gy)
+            particle_list[:] = [p for p in particle_list if p.is_alive]
 
-        particle_list[:] = [p for p in particle_list if p.is_alive]
+            screen.fill(pygame.Color("black"))
+            for p in particle_list:
+                p.draw(screen)
+            pygame.display.update()
 
-        screen.fill(pygame.Color("black"))
-        for p in particle_list:
-            p.draw(screen)
-        pygame.display.update()
-
-    pygame.quit()
+        pygame.quit()
 
 
 if __name__ == "__main__":
-    main()
+    app = AppMain()
+    app.run()
