3b717bd777b0f11ce62afa1624bd5d4f9b4817d9
Computer Seminar I <anonymous@example.org>
2023-11-13 15:02:56 +0900

    added multiple initial values

--- particle/test_particle.py
+++ particle/test_particle.py
@@ -1,11 +1,17 @@
+import pytest
 import particle
 
 
-def test_particle_move():
+@pytest.mark.parametrize("x0, y0, vx0, vy0", [
+    (0, 0, 5, 10),
+    (300, 200, -7, 12),
+    (500, 300, -4, -10),
+])
+def test_particle_move(x0, y0, vx0, vy0):
     width, height = 600, 400
     dt, g = 2, 4
     w = particle.World(width, height, dt, g)
-    x0, y0, vx0, vy0 = 0, 0, 5, 10
+
     p = particle.Particle((x0, y0), (vx0, vy0), w)
     p.update()
     assert p.x == x0 + vx0 * dt
